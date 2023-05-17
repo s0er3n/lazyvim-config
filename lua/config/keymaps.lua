d7b8f77674f1cbb8d0551a5e92df7377a6f027ef
Sören Michaels <soeren.michaels@gmail.com>
2023-05-17 07:44:55 +0200

    feat: doc string + harpoon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- helper:
 
 -- exiting terminal with esc
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+vim.api.nvim_set_keymap("x", "p", "P", { noremap = true })
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,9 +1,82 @@
 return {
   {
+    "pixelneo/vim-python-docstring",
+    keys = { { "<leader>cc", "<cmd>Docstring<CR>", desc = "add docstring" } },
+    ft = "python",
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+        "typescript-language-server",
+        "python-lsp-server",
+        "json-lsp",
+        "lua-language-server",
+        "prettier",
+        "python-lsp-server",
+        "rust-analyzer",
+        "svelte-language-server",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      {
+        "<C-e>",
+        function()
+          require("harpoon.ui").toggle_quick_menu()
+        end,
+        desc = "harpoon ui",
+      },
+      {
+        "<C-a>",
+        function()
+          require("harpoon.mark").add_file()
+        end,
+        desc = "harpoon add",
+      },
+      {
+        "<C-h>",
+        function()
+          require("harpoon.ui").nav_file(1)
+        end,
+        desc = "harpoon 1",
+      },
+      {
+        "<C-j>",
+        function()
+          require("harpoon.ui").nav_file(2)
+        end,
+        desc = "harpoon 2",
+      },
+      {
+        "<C-k>",
+        function()
+          require("harpoon.ui").nav_file(3)
+        end,
+        desc = "harpoon 3",
+      },
+      {
+        "<C-l>",
+        function()
+          require("harpoon.ui").nav_file(4)
+        end,
+        desc = "harpoon 4",
+      },
+    },
+  },
+  {
     "mbbill/undotree",
-    init = function()
-      vim.keymap.set("n", "<leader>o", vim.cmd.UndotreeToggle)
-    end,
+    keys = {
+      { "<leader>o", vim.cmd.UndotreeToggle, "UndoTree" },
+    },
   },
   {
     "codota/tabnine-nvim",
@@ -12,8 +85,8 @@ return {
     init = function()
       require("tabnine").setup({
         disable_auto_comment = true,
-        accept_keymap = "<Leader><Enter>",
-        dismiss_keymap = "<C-]>",
+        -- accept_keymap = "<C-<Enter>>",
+        -- dismiss_keymap = "<C-]>",
         debounce_ms = 800,
         suggestion_color = { gui = "#808080", cterm = 244 },
         exclude_filetypes = { "TelescopePrompt" },
